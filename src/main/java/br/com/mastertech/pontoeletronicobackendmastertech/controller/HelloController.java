6fcb86f22838c94975190277ee7825f4db827814
Tiago G Almeida <tiportal.11@gmail.com>
2020-06-06 03:13:56 -0300

    BREAKING CHANGE: Iniciando o projeto

---
+++ src/main/java/br/com/mastertech/pontoeletronicobackendmastertech/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.mastertech.pontoeletronicobackendmastertech.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+  @GetMapping(value="/")
+  public String getMethodName() {
+      return "OLA MUNDO";
+  }
+
+}
